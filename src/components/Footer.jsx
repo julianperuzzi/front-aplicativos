380d2afc440bc250a23b6240200b9276a3c56f97
Julian Peruzzi <peru964@gmail.com>
2024-07-02 18:44:06 -0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,16 +10,16 @@ const Footer = ({ darkMode }) => {
           <img className="h-16 w-16 mr-4" src={favicon} alt="Favicon" />
           <div>
             <p className="text-sm font-medium py-2">Julian Peruzzi | DNI 39955389</p>
-            <p className="text-l mb-2 ">Página de Login - materia Desarrollo de Aplicativos 2024</p>
+            <p className="text-sm mb-2">Página de Login - materia Desarrollo de Aplicativos 2024</p>
             <p className="text-sm mb-3">Universidad Católica de Cuyo - San Juan Argentina </p>
           </div>
-          <div className='px-24'>
+          <div className='px-24 hidden md:block'>
             <p className="text-sm font-medium ">Contacto</p>
             <p className="text-sm py-2">Email: julian.peruzzi@uc.cuyo.ar</p>
             <p className="text-sm ">Teléfono: 0291 154 7632</p>
           </div>
-          <div>
-            <p className="text-sm font-medium ">Redes Sociales</p>
+          <div className='px-24 hidden md:block'>
+            <p className="text-sm font-medium">Redes Sociales</p>
             <p href="" target="_blank" rel="noopener noreferrer" className="text-sm py-2">LinkedIn</p>
             <p href="" target="_blank" rel="noopener noreferrer" className="text-sm ">Instagram</p>
           </div>
